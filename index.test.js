077dafee89d0073eca1282e4c46b04627c17506b
Kirera Wainaina <richardwainainak@gmail.com>
2023-02-07 16:39:33 +0300

    Write test for function that checks if its API route

--- index.test.js
+++ index.test.js
@@ -15,3 +15,14 @@ test('Redirect HTTP Requests', () => {
     expect(response.writeHead.mock.calls[0][0]).toBe('301')
     expect(response.end.mock.calls).toHaveLength(1)
 })
+
+test('Is API route', () => {
+    const a = "abs/api/food", b = "/api/food/",
+    c = "/api/food/dinner", d = "api", e = "/api/";
+
+    expect(index.isAPIRoute(a)).toBeFalsy();
+    expect(index.isAPIRoute(b)).toBeTruthy();
+    expect(index.isAPIRoute(c)).toBeTruthy();
+    expect(index.isAPIRoute(d)).toBeFalsy();
+    expect(index.isAPIRoute(e)).toBeFalsy();
+})
