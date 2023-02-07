6704529d5e391c2d19d0b4d7d9983cede2a92567
Kirera Wainaina <richardwainainak@gmail.com>
2023-02-07 14:11:10 +0300

    Test if http requests are redirected

--- index.test.js
+++ index.test.js
@@ -7,3 +7,13 @@ test('Creates log message', () => {
     expect(index.createLogMessage(request))
         .toBe(`${new Date}, ${request.method}, ${request.url}`)
 })
+
+test('Redirect HTTP Requests', () => {
+    const response = {};
+    response.writeHead = jest.fn();
+    response.end = jest.fn();
+
+    index.redirectHTTPRequests(response);
+    expect(response.writeHead.mock.calls[0][0]).toBe('301')
+    expect(response.end.mock.calls).toHaveLength(1)
+})
