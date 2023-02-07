131370394411040374eaee069797772226c7fadc
Kirera Wainaina <richardwainainak@gmail.com>
2023-02-07 13:48:10 +0300

    Check if index file is running as module
    To prevent it from starting a server during testing

--- index.js
+++ index.js
@@ -2,17 +2,21 @@ const http = require('http');
 
 const httpPort = 80;
 
-const httpServer = http.createServer();
-httpServer.listen(httpPort, 
-    () => console.log(`Server listening on port ${httpPort}`));
-
-httpServer.on('request', handleHTTPRequest);
+if (require.main == module) {
+    const httpServer = http.createServer();
+    httpServer.listen(httpPort, 
+        () => console.log(`Server listening on port ${httpPort}`));
+    
+    httpServer.on('request', handleHTTPRequest);
+}
 
 function handleHTTPRequest(request, response) {
-
+    //console.log(createLogMessage(request));
+    console.log(require.main)
 }
 
-exports.printLog = function printLog(request) {
-    // log message of the format
-    // date, request method, path
+function createLogMessage(request) {
+    //return `${new Date}, ${request.method}, ${request.url}`
 }
+
+exports.createLogMessage = createLogMessage;
