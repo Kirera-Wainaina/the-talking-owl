e12d8d1cfedd3efaef3c141c9702e055e97392e0
Kirera Wainaina <richardwainainak@gmail.com>
2023-02-09 14:51:34 +0300

    Check if password and repeat password match

--- frontend/js/admin-signup.js
+++ frontend/js/admin-signup.js
@@ -1,11 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
     const signupForm = document.querySelector('#signup');
-    signupForm.addEventListener('submit', handleSubmit);    
+    signupForm.addEventListener('submit', handleSignupSubmit);    
 })
 
 
-function handleSubmit(event) {
+function handleSignupSubmit(event) {
     event.preventDefault();
 
-    console.log('called')
+    if (!passwordsMatch())
+}
+
+function passwordsMatch() {
+    const password = document.querySelector('input[name="password"]').value;
+    const repeatPassword = document.querySelector('input[name="repeatPassword"]').value;
+
+    if (password === repeatPassword) return true;
+    return false
 }
