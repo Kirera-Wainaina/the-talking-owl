d032e55ade2f99ed3f997514488c712914f293cc
Kirera Wainaina <richardwainainak@gmail.com>
2023-02-10 18:02:03 +0300

    Save data to Firestore

---
+++ utils/database.js
@@ -0,0 +1,14 @@
+const { Firestore } = require('@google-cloud/firestore');
+const dotenv = require('dotenv');
+
+dotenv.config()
+
+const firestore = new Firestore({
+    projectId: process.env.PROJECT_ID,
+    keyFilename: process.env.SERVICE_ACCOUNT
+});
+
+exports.saveData = function(data, collectionName) {
+    const collection = firestore.collection(collectionName);
+    return collection.add(data)
+}
