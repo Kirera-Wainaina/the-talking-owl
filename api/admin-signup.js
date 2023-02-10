9c1a8e8c7b0721275f6cfd7915d73ccffa249ac4
Kirera Wainaina <richardwainainak@gmail.com>
2023-02-10 17:53:08 +0300

    Write code to create the data to save to database

--- api/admin-signup.js
+++ api/admin-signup.js
@@ -2,20 +2,39 @@ const bcrypt = require('bcrypt')
 const dotenv = require('dotenv');
 
 const FormDataHandler = require('../utils/formDataHandler');
-const responder = require('../utils/responder')
+const responder = require('../utils/responder');
+const database = require('../utils/database')
 dotenv.config()
 
-module.exports = async function(request, response) {
-    const [fields, files] = await new FormDataHandler(request).run();
+exports.main = async function(request, response) {
+    const [fields] = await new FormDataHandler(request).run();
 
     if (!await isAdminPassword(fields['adminPassword'])) {
         responder.httpResponse(response, 'unauthorized');
         return
     }
+
+    createDataToSave(fields)
 }
 
 function isAdminPassword(entry) {
     return bcrypt.compare(entry, process.env.ADMIN_PASSWORD);
 }
 
-exports.isAdminPassword = isAdminPassword;
+async function createDataToSave(fields) {
+    return {
+        email: fields.email,
+        firstName: fields.firstName,
+        password: await hashPassword(fields.password)
+    }
+}
+
+function hashPassword(password) {
+    return new Promise((resolve, reject) => {
+        bcrypt.hash(password, 12)
+            .then(result => resolve(result))
+            .catch(error => reject(error))
+    })
+}
+
+exports.createDataToSave = createDataToSave;
