1c25e16dcd20890f0433dbcf41013a2eaeacb7ba
Kirera Wainaina <richardwainainak@gmail.com>
2023-02-12 13:49:13 +0300

    Verify password after retrieving user data
    Send error if passwords don't match. Redirect to home page if they
    do.

--- api/admin-login.js
+++ api/admin-login.js
@@ -1,21 +1,37 @@
 const FormDataHandler = require('../utils/formDataHandler');
 const responder = require('../utils/responder');
-const database = require('../utils/database')
+const database = require('../utils/database');
+const bcrypt = require('bcrypt');
 
 
 exports.main = async function(request, response) {
     try {
         const [fields] = await new FormDataHandler(request).run();
 
-        const user = await database.getUserByEmail(fields.email, 'admins');
-        if (user.empty) {
+        const querySnapshot = await database.getUserByEmail(fields.email, 'admins');
+        if (querySnapshot.empty) {
             responder.httpResponse(response, 'unauthorized');
             return ;
         }
-        console.log(user.size)
-        // user.forEach(doc => console.log(doc.data()))
+
+        querySnapshot.forEach(user => handlePasswordVerification(
+                response, 
+                user.data().password, 
+                fields.password))
     } catch (error) {
         console.log(error);
         responder.httpResponse(response, 'error')
     }
+}
+
+async function handlePasswordVerification(response, hash, plainPassword) {
+    if (await isPassword(plainPassword, hash)) {
+        responder.httpResponse(response, 'success')
+    } else {
+        responder.httpResponse(response, 'unauthorized')
+    }
+}
+
+function isPassword(plainPassword, hash) {
+    return bcrypt.compare(plainPassword, hash)
 }
