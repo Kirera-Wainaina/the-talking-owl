7a71c5b3d9ab7d17c7c6ebca6b36475cd4e681ad
Kirera Wainaina <richardwainainak@gmail.com>
2023-02-13 17:54:06 +0300

    Import render function from article js

--- frontend/js/admin/upload-article.js
+++ frontend/js/admin/upload-article.js
@@ -1,11 +1,25 @@
+import { render } from '../article.js';
+
 document.addEventListener('DOMContentLoaded', () => {
     const checkPreviewButton = document.getElementById('check-preview');
     const exitPreviewButton = document.getElementById('exit-preview');
 
-    checkPreviewButton.addEventListener('click', displayPreview)
+    checkPreviewButton.addEventListener('click', togglePreview)
+    checkPreviewButton.addEventListener('click', () => render(createPreviewData()))
+    exitPreviewButton.addEventListener('click', togglePreview);
 })
 
-function displayPreview() {
+function togglePreview() {
     const preview = document.getElementById('preview');
     preview.classList.toggle('hide');
+}
+
+function createPreviewData() {
+    return {
+        title: document.querySelector('input[name="title"]').value,
+        category: document.querySelector('select[name="category"]').value,
+        description: document.querySelector('input[name="description"]').value,
+        coverPhoto: document.querySelector('input[name="coverPhoto"]').value,
+        content: document.querySelector('textarea').value
+    };
 }
