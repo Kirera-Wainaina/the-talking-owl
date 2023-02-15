84604d40f4354cb7f83f7824acc524b9fe11a165
Kirera Wainaina <richardwainainak@gmail.com>
2023-02-15 13:47:21 +0300

    Exit preview button is part of body not #preview container

--- frontend/js/admin/upload-article.js
+++ frontend/js/admin/upload-article.js
@@ -3,18 +3,28 @@ import { render } from '../article.js';
 document.addEventListener('DOMContentLoaded', () => {
     const checkPreviewButton = document.getElementById('check-preview');
     const exitPreviewButton = document.getElementById('exit-preview');
-
-    checkPreviewButton.addEventListener('click', togglePreview)
+    
+    checkPreviewButton.addEventListener('click', togglePreview);
     checkPreviewButton.addEventListener('click', 
-        () => render(document.getElementById('preview'), 
-            createPreviewData()))
+        () => togglePreviewButtons(checkPreviewButton, exitPreviewButton));
+    checkPreviewButton.addEventListener('click', 
+        () => render(document.getElementById('preview'), createPreviewData()))
 
     exitPreviewButton.addEventListener('click', togglePreview);
+    exitPreviewButton.addEventListener('click', 
+        () => togglePreviewButtons(checkPreviewButton, exitPreviewButton));
 })
+
+
 
 function togglePreview() {
     const preview = document.getElementById('preview');
     preview.classList.toggle('hide');
+}
+
+function togglePreviewButtons(checkPreview, exitPreview) {
+    checkPreview.classList.toggle('hide');
+    exitPreview.classList.toggle('hide')
 }
 
 function createPreviewData() {
