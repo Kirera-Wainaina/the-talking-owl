cb2fd59a0bee9b057a38ce07e6e13601977c8b74
Kirera Wainaina <richardwainainak@gmail.com>
2023-02-16 17:40:19 +0300

    Add click listener for upload button

---
+++ frontend/js/admin/image-library.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const openUploadButtons = document.querySelectorAll('.open-upload');
+    openUploadButtons.forEach(
+        button => button.addEventListener('click', toggleUploadContainer));
+})
+
+function toggleUploadContainer() {
+    const modal = document.getElementById('upload-modal');
+    modal.classList.toggle('hide');
+}
