dbf9e05997b501d66c95d5647b518fef55d930a1
Kirera Wainaina <richardwainainak@gmail.com>
2023-02-16 18:08:03 +0300

    Close upload container when close button is clicked

--- frontend/js/admin/image-library.js
+++ frontend/js/admin/image-library.js
@@ -2,6 +2,9 @@ document.addEventListener('DOMContentLoaded', () => {
     const openUploadButtons = document.querySelectorAll('.open-upload');
     openUploadButtons.forEach(
         button => button.addEventListener('click', toggleUploadContainer));
+
+    const closeButton = document.getElementById('close-icon');
+    closeButton.addEventListener('click', toggleUploadContainer)
 })
 
 function toggleUploadContainer() {
