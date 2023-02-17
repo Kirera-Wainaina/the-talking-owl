fe244166b587a8e05a71f800fc60dc3a81b6c6e4
Kirera Wainaina <richardwainainak@gmail.com>
2023-02-17 13:16:35 +0300

    click on file input when button is clicked

--- frontend/js/admin/image-library.js
+++ frontend/js/admin/image-library.js
@@ -11,3 +11,13 @@ function toggleUploadContainer() {
     const modal = document.getElementById('upload-modal');
     modal.classList.toggle('flex');
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    const chooseFileButton = document.getElementById('choose-file');
+    chooseFileButton.addEventListener('click', clickFileInput);
+})
+
+function clickFileInput() {
+    const input = document.getElementById('choose-file-input');
+    input.click();
+}
