2b710b5e9ec8eac18c7a897345b33b499abee6e8
Kirera Wainaina <richardwainainak@gmail.com>
2023-02-17 13:43:45 +0300

    Display the images to upload

--- frontend/js/admin/image-library.js
+++ frontend/js/admin/image-library.js
@@ -13,11 +13,29 @@ function toggleUploadContainer() {
 }
 
 document.addEventListener('DOMContentLoaded', () => {
+    const input = document.getElementById('choose-file-input');
+
     const chooseFileButton = document.getElementById('choose-file');
-    chooseFileButton.addEventListener('click', clickFileInput);
+    chooseFileButton.addEventListener('click', () => clickFileInput(input));
+
+    input.addEventListener('change', displayImages)
 })
 
-function clickFileInput() {
-    const input = document.getElementById('choose-file-input');
+function clickFileInput(input) {
     input.click();
+}
+
+function displayImages(event) {
+    const container = document.getElementById('images-to-upload');
+    container.appendChild(createImageElements(event.target.files))
+}
+
+function createImageElements(files) {
+    const fragment = new DocumentFragment();
+    for (const file of files) {
+        const imgEl = document.createElement('img')
+        imgEl.src = URL.createObjectURL(file);
+        fragment.append(imgEl)
+    }
+    return fragment
 }
