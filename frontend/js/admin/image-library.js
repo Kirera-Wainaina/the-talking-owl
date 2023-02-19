9273aee7c70d36f0a981b8eaba1c0d9ee730da0e
Kirera Wainaina <richardwainainak@gmail.com>
2023-02-19 13:12:48 +0300

    input the image to upload in a div container with close icon

--- frontend/js/admin/image-library.js
+++ frontend/js/admin/image-library.js
@@ -27,15 +27,37 @@ function clickFileInput(input) {
 
 function displayImages(event) {
     const container = document.getElementById('images-to-upload');
-    container.appendChild(createImageElements(event.target.files))
+    container.appendChild(createImageContainers(event.target.files))
 }
 
-function createImageElements(files) {
+function createImageContainers(files) {
     const fragment = new DocumentFragment();
     for (const file of files) {
-        const imgEl = document.createElement('img')
-        imgEl.src = URL.createObjectURL(file);
-        fragment.append(imgEl)
+        fragment.append(createSingleImageContainer(file))
     }
     return fragment
+}
+
+function createSingleImageContainer(file) {
+    const div = document.createElement('div');
+    const imgEl = createImageElement(URL.createObjectURL(file))
+    const input = createCloseIconElement();
+
+    div.append(input);
+    div.append(imgEl);
+    return div;
+}
+
+function createImageElement(src) {
+    const imgEl = document.createElement('img');
+    imgEl.src = src;
+    return imgEl
+}
+
+function createCloseIconElement() {
+    const input = document.createElement('input');
+    input.src = "/frontend/images/close_icon.svg";
+    input.type = 'image';
+    input.classList.add('uploaded-image-close-icon');
+    return input;
 }
