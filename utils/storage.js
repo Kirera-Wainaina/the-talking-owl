5a8475907eca343dd761cca2936a7732c9cefd50
Kirera Wainaina <richardwainainak@gmail.com>
2023-02-20 07:41:04 +0300

    create a function to save images to storage bucket

---
+++ utils/storage.js
@@ -0,0 +1,21 @@
+const { Storage } = require('@google-cloud/storage');
+const path = require('path');
+const fs = require('fs');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const storage = new Storage({ keyFilename: process.env.SERVICE_ACCOUNT });
+
+function saveImage(filePath, bucketName=process.env.BUCKET_NAME) {
+    const bucket = storage.bucket(bucketName);
+    const file = bucket.file(path.basename(filePath));
+
+    return new Promise((resolve, reject) => {
+        fs.createReadStream(filePath)
+            .pipe(file.createWriteStream())
+            .on('error', error => reject(error))
+            .on('finish', () => resolve());
+    })
+}
+
+exports.saveImage = saveImage;
