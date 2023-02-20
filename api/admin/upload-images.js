68678d303c5826b40e228887b7f6ab6d115e6235
Kirera Wainaina <richardwainainak@gmail.com>
2023-02-20 07:56:23 +0300

    delete images after saving them to cloud

--- api/admin/upload-images.js
+++ api/admin/upload-images.js
@@ -3,11 +3,18 @@ const storage = require('../../utils/storage');
 const imagemin = require('imagemin');
 const imageminWebp = require('imagemin-webp');
 const path = require('path');
+const fsPromises = require('fs/promises');
 
 exports.main = async function(request, response) {
     const [fields, files] = await new FormDataHandler(request).run();
-    const convertedFilePaths = await Promise.all(files.map(filePath => minimizeImage(filePath)));
-    await Promise.all(convertedFilePaths.map(filePath => storage.saveImage(filePath[0].destinationPath)))
+    const convertedFileMetadata = await Promise.all(files.map(filePath => minimizeImage(filePath)));
+    
+    await Promise.all(convertedFileMetadata.map(
+        filePath => storage.saveImage(filePath[0].destinationPath)));
+
+    await Promise.all(convertedFileMetadata.map(
+        async metadata => await Promise.all([deleteImage(metadata[0].destinationPath), 
+                                            deleteImage(metadata[0].sourcePath)])));
     console.log('done')
 }
 
@@ -18,4 +25,8 @@ function minimizeImage(filePath) {
         destination,
         plugins: [imageminWebp({quality: 70})]
     })
+}
+
+function deleteImage(filePath) {
+    return fsPromises.unlink(filePath);
 }
