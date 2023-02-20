a296826ca636fc618c968d7e09e32c7425c74b64
Kirera Wainaina <richardwainainak@gmail.com>
2023-02-20 07:20:37 +0300

    minimize images using imagemin to webp format

--- api/admin/upload-images.js
+++ api/admin/upload-images.js
@@ -1,6 +1,20 @@
 const FormDataHandler = require('../../utils/formDataHandler');
+const imagemin = require('imagemin');
+const imageminWebp = require('imagemin-webp');
+const path = require('path');
 
 exports.main = async function(request, response) {
     const [fields, files] = await new FormDataHandler(request).run();
-    console.log(files)
+    const convertedFilePaths = await Promise.all(files.map(filePath => minimizeImage(filePath)));
+    // upload these files to the cloud
+    console.log(convertedFilePaths);
+}
+
+function minimizeImage(filePath) {
+    const destination = path.join(path.dirname(path.dirname(__dirname)), 'converted');
+
+    return imagemin([filePath], {
+        destination,
+        plugins: [imageminWebp({quality: 70})]
+    })
 }
