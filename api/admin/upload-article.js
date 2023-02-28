79d9364e59d237ed93cb2519bc9fe2c62f2477c9
Kirera Wainaina <richardwainainak@gmail.com>
2023-02-28 18:17:38 +0300

    save data and send response

--- api/admin/upload-article.js
+++ api/admin/upload-article.js
@@ -1,7 +1,19 @@
 const FormDataHandler = require('../../utils/formDataHandler');
+const database = require('../../utils/database')
 
 exports.main = async function(request, response) {
-    const [fields] = await new FormDataHandler(request).run();
+    try {
+        const [fields] = await new FormDataHandler(request).run();
+        const doc = await database.saveData(fields, 'articles');
 
-    console.log(fields);
+        if (doc.id) {
+            console.log('Data was saved successfully!')
+            response.writeHead(200, {'content-type': 'text/plain'})
+            response.end('success')
+        }
+    } catch (error) {
+        console.log(error);
+        response.writeHead(500, {'content-type': 'text/plain'})
+        response.end('error')
+    }
 }
