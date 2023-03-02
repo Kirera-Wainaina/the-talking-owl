6fa1f7d91f6a6399161c3208c4d7b674352cdc4b
Kirera Wainaina <richardwainainak@gmail.com>
2023-03-02 18:13:25 +0300

    iterate through the data while creating a container

--- frontend/js/admin/edit-articles.js
+++ frontend/js/admin/edit-articles.js
@@ -1,11 +1,18 @@
+import { createArticleContainer } from "../article-list.js";
+
 document.addEventListener('DOMContentLoaded', retreiveArticleData);
 
 function retreiveArticleData() {
-    fetch('/api/articles?field=description&field=landscapeImage&field=\
-    publishedDate&orderBy=publishedDate&orderByDirection=desc')
-        .then(response => displayArticleList(response.json()))
+    fetch('/api/articles?field=title&field=description&field=landscapeImage&field=\
+publishedDate&orderBy=publishedDate&orderByDirection=desc')
+        .then(response => response.json())
+        .then(data => displayArticleList(data))
 }
 
 function displayArticleList(data) {
-    
+    console.log(data)
+    const fragment = new DocumentFragment();
+    data.forEach(article => {
+        fragment.append(createArticleContainer(article));
+    });
 }
