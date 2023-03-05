0889823f1bea157ffb524533c023c211a9a8727f
Kirera Wainaina <richardwainainak@gmail.com>
2023-03-05 13:35:25 +0300

    input data into the input elements

--- frontend/js/admin/edit.js
+++ frontend/js/admin/edit.js
@@ -4,5 +4,14 @@ function fetchArticleData() {
     const params = new URLSearchParams(location.search);
     fetch(`/api/articles?urlTitle=${params.get('urlTitle')}&id=${params.get('id')}`)
         .then(response => response.json())
-        .then(data => console.log(data))
+        .then(data => inputDataIntoForm(data[0]));
+}
+
+function inputDataIntoForm(data) {
+    inputDataIntoInputElements(data);
+}
+
+function inputDataIntoInputElements(data) {
+    const inputs = document.querySelectorAll('form input');
+    inputs.forEach(input => input.value = data[input.name]);
 }
