c7bc5465c554a9340fa35777ae02fe3517465e4b
Kirera Wainaina <richardwainainak@gmail.com>
2023-03-05 11:52:46 +0300

    check whether an article title is used to avoid duplicate titles

--- api/admin/upload-article.js
+++ api/admin/upload-article.js
@@ -4,16 +4,34 @@ const database = require('../../utils/database')
 exports.main = async function(request, response) {
     try {
         const [fields] = await new FormDataHandler(request).run();
-        const doc = await database.saveData(fields, 'articles');
 
-        if (doc.id) {
-            console.log('Data was saved successfully!')
-            response.writeHead(200, {'content-type': 'text/plain'})
-            response.end('success')
+        if (await urlTitleExists(fields.urlTitle)) { // if urlTitle exists, don't save
+            console.log('urlTitle exists so article not saved');
+            response.writeHead(500, {'content-type': 'text/plain'})
+            response.end('url-exists')    
+        } else {
+            const doc = await database.saveData(fields, 'articles');
+
+            if (doc.id) {
+                console.log('Data was saved successfully!')
+                response.writeHead(200, {'content-type': 'text/plain'})
+                response.end('success')
+            }
         }
     } catch (error) {
         console.log(error);
         response.writeHead(500, {'content-type': 'text/plain'})
         response.end('error')
     }
+}
+
+function urlTitleExists(urlTitle) {
+    const collection = database.firestore.collection('articles');
+    return collection
+        .where('urlTitle', '==', urlTitle)
+        .get()
+        .then(querySnapshot => {
+            if (querySnapshot.empty) return false;
+            return true
+        })
 }
