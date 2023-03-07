1145a4d32873acad31c24d89f462702f4f62b6eb
Kirera Wainaina <richardwainainak@gmail.com>
2023-03-07 17:59:46 +0300

    query for number of articles in the business category

--- frontend/js/business.js
+++ frontend/js/business.js
@@ -2,6 +2,8 @@ import { displayArticleList } from "./article-list.js";
 
 document.addEventListener('DOMContentLoaded', retrieveArticleData);
 
+document.addEventListener('DOMContentLoaded', displayNumberOfPages);
+
 function retrieveArticleData() {
     fetch('/api/articles?field=title&field=description&field=landscapeImage&field=\
 publishedDate&field=urlTitle&category=business&orderBy=publishedDate&orderByDirection=desc')
@@ -15,4 +17,14 @@ publishedDate&field=urlTitle&category=business&orderBy=publishedDate&orderByDire
 
 function createArticleHref(urlTitle, id) {
     return `/article/${urlTitle}?id=${id}`
+}
+
+async function displayNumberOfPages() {
+    const count = await retrieveNumberOfPages();
+    console.log(count);
+}
+
+function retrieveNumberOfPages() {
+    return fetch('/api/articles?category=business&count=true')
+        .then(response => response.json())
 }
