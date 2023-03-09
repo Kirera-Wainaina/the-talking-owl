c91205d9c5146a51ea63ed8b887b4fd3b84d50d9
Kirera Wainaina <richardwainainak@gmail.com>
2023-03-09 11:42:05 +0300

    create a technology js file

---
+++ frontend/js/technology.js
@@ -0,0 +1,31 @@
+import { displayArticleList, getCurrentPageNumber, displayPageNumbers } from "./article-list.js";
+
+document.addEventListener('DOMContentLoaded', retrieveArticleData);
+
+document.addEventListener(
+    'DOMContentLoaded', 
+    async () => displayPageNumbers(await retrieveBusinessArticleCount()));
+
+function retrieveArticleData() {
+    const pageNumber = getCurrentPageNumber();
+    const offset = (pageNumber -1) * 10;
+    
+    fetch(`/api/articles?field=title&field=description&field=landscapeImage&field=\
+publishedDate&field=urlTitle&field=landscapeImageText&category=tech\
+&orderBy=publishedDate&orderByDirection=desc&limit=10&offset=${offset}`)
+        .then(response => response.json())
+        .then(data => displayArticleList(
+            document.getElementById('article-list'),
+            data,
+            createArticleHref
+        ))
+}
+
+function createArticleHref(urlTitle, id) {
+    return `/article/${urlTitle}?id=${id}`
+}
+
+function retrieveBusinessArticleCount() {
+    return fetch('/api/articles?category=tech&count=true')
+        .then(response => response.json())
+}
