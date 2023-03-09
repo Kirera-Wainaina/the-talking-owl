1adf387aeb9cd95eb9ec597cd82b601b09151865
Kirera Wainaina <richardwainainak@gmail.com>
2023-03-09 13:14:41 +0300

    Import article data for rendering

--- frontend/js/article.js
+++ frontend/js/article.js
@@ -1,9 +1,27 @@
-export function render(parentContainer, data) {
-    let html = '';
-    html += createTitleElement(data.title);
-    parentContainer.innerHTML = html;
+import { render } from './render.js';
+
+document.addEventListener('DOMContentLoaded', async () => {
+    const container = document.querySelector('.render');
+    const [ data ] = await retrieveArticle();
+    render(container, data);
+})
+
+function retrieveArticle() {
+    return fetch(`/api/articles?field=content&field=description&field=landscapeImage\
+&field=landscapeImageText&field=portraitImage&field=portraitImageText\
+&field=publishedDate&field=title&id=${getArticleId()}&urlTitle=${getUrlTitle()}`)
+    .then(response => response.json())
 }
 
-function createTitleElement(title) {
-    return `<h1>${title}</h1>`
+function getUrlTitle() {
+    const [ urlTitle ] = location.pathname.match(/(?<=\/article\/).*/);
+    return urlTitle;
 }
+
+function getArticleId() {
+    const params = new URLSearchParams(location.search);
+    return params.get('id');
+}
+
+// retrieveArticle()
+//     .then(data => console.log(data));
