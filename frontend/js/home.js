e318ff2077d37ac58876511cd3584858f933a99e
Kirera Wainaina <richardwainainak@gmail.com>
2023-03-13 10:52:14 +0300

    fetch business articles

--- frontend/js/home.js
+++ frontend/js/home.js
@@ -1,12 +1,15 @@
-import { fetchData } from "./general.js";
-
 document.addEventListener('DOMContentLoaded', displayBusinessArticles);
 
 async function displayBusinessArticles() {
-    const data = await fetchData('/api/articles?field=squareThumbnail\
+    const data = await retrieveBusinessArticles();
+    console.log(data);
+}
+
+function retrieveBusinessArticles() {
+    return fetch('/api/articles?field=squareThumbnail\
 &field=squareThumbnailText&field=title&field=description\
 &field=publishedDate&field=urlTitle&orderBy=publishedDate\
-&orderByDirection=desc&category=business&limit=4');
-    console.log(data);
+&orderByDirection=desc&category=business&limit=4')
+    .then(response => response.json());   
 }
 
