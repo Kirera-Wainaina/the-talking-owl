e51243ec0d36f7e208b457982257683455084842
Kirera Wainaina <richardwainainak@gmail.com>
2023-03-13 08:55:40 +0300

    write function that takes url and returns data

--- frontend/js/general.js
+++ frontend/js/general.js
@@ -80,6 +80,10 @@ export function getArticleId(url=location.href) {
     return params.get('id');
 }
 
+export function fetchData(url) {
+    return fetch(url)
+        .then(response => response.json());
+}
 
 document.addEventListener('DOMContentLoaded', () => {
     // toggle menu sidebar on phone viewport
