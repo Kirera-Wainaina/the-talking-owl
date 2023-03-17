1cadd7e0757183f44ec17dd9498ef79c7c6b5045
Kirera Wainaina <richardwainainak@gmail.com>
2023-03-17 16:06:41 +0300

    pass urlTitle and id to render function

--- api/admin/edit-article.js
+++ api/admin/edit-article.js
@@ -1,12 +1,17 @@
 const FormDataHandler = require('../../utils/formDataHandler');
 const database = require('../../utils/database');
+const { renderAllPages } = require('../../utils/serverRender');
 
 exports.main = async function(request, response) {
     try {
         const [fields] = await new FormDataHandler(request).run();
+        const urlTitle = fields.urlTitle;
+        const id = fields.id;
 
         const result = await editArticle(fields)
+        
         if (result.writeTime) {
+            renderAllPages(urlTitle, id, fields.category);
             console.log('Data was edited successfully!')
                 response.writeHead(200, {'content-type': 'text/plain'})
                 response.end('success')
@@ -22,6 +27,7 @@ exports.main = async function(request, response) {
 function editArticle(data) {
     const id = data.id;
     delete data.id;
+    delete data.urlTitle
     return database.firestore.doc(`articles/${id}`)
         .update(data)
 }
