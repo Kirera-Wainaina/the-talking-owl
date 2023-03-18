f89c1e9e49306d454fe5aeecfe33ac7868593cc0
Kirera Wainaina <richardwainainak@gmail.com>
2023-03-18 18:36:57 +0300

    enter the og element data through js

--- frontend/js/article.js
+++ frontend/js/article.js
@@ -6,14 +6,15 @@ document.addEventListener('DOMContentLoaded', async () => {
     const [ data ] = await retrieveArticle();
     renderOnArticlePage(data);
     fillStructuredData(data);
+    fillOGElements(data);
 })
 
 function retrieveArticle() {
     return fetch(`/api/articles?field=content&field=description\
 &field=landscapeImage&field=landscapeImageText&field=portraitImage\
 &field=portraitImageText&field=publishedDate&field=title\
-&field=relatedArticle1&field=relatedArticle2&id=${getArticleId()}\
-&urlTitle=${getArticleUrlTitle()}`)
+&field=relatedArticle1&field=relatedArticle2&field=category\
+&id=${getArticleId()}&urlTitle=${getArticleUrlTitle()}`)
     .then(response => response.json())
 }
 
@@ -30,4 +31,55 @@ function fillStructuredData(article) {
 
 function convertMillisecondsToISO(milliseconds) {
     return new Date(Number(milliseconds)).toISOString();
+}
+
+function fillOGElements(article) {
+    setOGTitle(article.title);
+    setOGPublishedTime(article.publishedDate);
+    setOGArticleAuthor('Kirera Wainaina');
+    setOGArticleSection(article.category);
+    setOGArticleImage(article.landscapeImage);
+    setOGArticleImageAltText(article.landscapeImageText);
+    setOGArticleUrl();
+    setOGArticleDescription(article.description);
+}
+
+function setOGTitle(title) {
+    const OGTitle = document.querySelector('meta[name="og:title"]');
+    OGTitle.setAttribute('content', title);
+}
+
+function setOGPublishedTime(timeInMilliseconds) {
+    const element = document.querySelector('meta[name="og:article:published_time"]');
+    element.setAttribute('content', convertMillisecondsToISO(timeInMilliseconds));
+}
+
+function setOGArticleAuthor(name) {
+    const element = document.querySelector('meta[name="og:article:author"]');
+    element.setAttribute('content', name);
+}
+
+function setOGArticleSection(category) {
+    const element = document.querySelector('meta[name="og:article:section"]');
+    element.setAttribute('content', category);
+}
+
+function setOGArticleImage(imageUrl) {
+    const element = document.querySelector('meta[name="og:image"]');
+    element.setAttribute('content', imageUrl);
+}
+
+function setOGArticleImageAltText(imageAltText) {
+    const element = document.querySelector('meta[name="og:image:alt" ]');
+    element.setAttribute('content', imageAltText);
+}
+
+function setOGArticleUrl() {
+    const element = document.querySelector('meta[name="og:url"]');
+    element.setAttribute('content', location.href);
+}
+
+function setOGArticleDescription(description) {
+    const element = document.querySelector('meta=[name="og:description"]');
+    element.setAttribute('content', description);
 }
