dff21fca6a36893846195e63e8ea968cd2702223
Kirera Wainaina <richardwainainak@gmail.com>
2023-03-18 14:22:04 +0300

    display a slider upon receiving a response

--- frontend/js/admin/home.js
+++ frontend/js/admin/home.js
@@ -3,12 +3,31 @@ import { showSpinningIcon, toggleElementClass  } from "../general.js";
 const renderButton = document.querySelector('#menu button');
 renderButton.addEventListener('click', renderAllPages);
 
-function renderAllPages(event) {
+async function renderAllPages(event) {
     showLoadingModal();
+    const response = await submitRenderAllPagesRequest();
+    hideLoadingModal();
+    if (response == 'success') {
+        toggleElementClass(document.getElementById('render-success-slider'), 'hide')
+    } else {
+        toggleElementClass(document.getElementById('render-error-slider'), 'hide')
+    }
 }
 
 function showLoadingModal() {
     const modal = document.getElementById('modal');
     toggleElementClass(modal, 'hide');
     showSpinningIcon(modal);
+}
+
+function hideLoadingModal() {
+    const modal = document.getElementById('modal');
+    toggleElementClass(modal, 'hide');
+}
+
+function submitRenderAllPagesRequest() {
+    return fetch(
+        '/api/admin/render-all-pages',
+        { method: 'POST'}
+    ).then(response => response.text())
 }
