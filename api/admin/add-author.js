c534b20db4b63699055034131a2f4e2d41815087
Kirera Wainaina <richardwainainak@gmail.com>
2023-04-21 17:53:00 +0300

    save profile image to uploaded folder

--- api/admin/add-author.js
+++ api/admin/add-author.js
@@ -1,21 +1,24 @@
 const FormDataHandler = require('../../utils/formDataHandler');
 const storage = require('../../utils/storage');
+const imageHandler = require('../../utils/imageHandler');
+const bcrypt = require('bcrypt');
+const fsPromises = require('fs/promises');
 
 exports.main = async function (request, response) {
     try {
         const [fields, files] = await new FormDataHandler(request).run();
         if (await isAdminPassword(fields.adminPassword)) {
-            console.log(files)
-            console.log(fields)
-            const cloudFile = await storage.saveImage(files[0]);
-            const metadata = await storage.getFileMetadata(cloudFile);                
+            console.log(files);
+            console.log(files);
         } else {
+            await fsPromises.unlink(files[0])
             response.writeHead(403, {'content-type': 'text/html' });
             response.end('forbidden');
         }
 
     } catch (error) {
         console.log(error);
+        await fsPromises.unlink(files[0])
         response.writeHead(500, { 'content-type': 'text/html'});
         response.end('error')
     }
@@ -23,4 +26,14 @@ exports.main = async function (request, response) {
 
 function isAdminPassword(entry) {
     return bcrypt.compare(entry, process.env.ADMIN_PASSWORD);
+}
+
+function createDataObject(fields, metadata) {
+    delete fileNumber;
+    delete adminPassword;
+    return {
+        ...fields,
+        profileImageName: metadata.name,
+        profileImageLink: metadata.mediaLink
+    }
 }
