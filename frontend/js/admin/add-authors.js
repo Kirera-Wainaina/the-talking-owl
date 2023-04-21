cac3f8e05c59869cd8c19cc0fa2aa9bfcc6a217e
Kirera Wainaina <richardwainainak@gmail.com>
2023-04-21 11:29:34 +0300

    display the profile image to upload

--- frontend/js/admin/add-authors.js
+++ frontend/js/admin/add-authors.js
@@ -1,5 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
-    attachListenerToFileButton()
+    attachListenerToFileButton();
+
+    const fileInput = document.querySelector('input[type="file"]');
+    fileInput.addEventListener('change', displayProfileImage);
 })
 
 function attachListenerToFileButton() {
@@ -7,4 +10,11 @@ function attachListenerToFileButton() {
     const addImageButton = document.querySelector('input[value="Add Image"]');
     
     addImageButton.onclick = () => fileInput.click();
+}
+
+function displayProfileImage(event) {
+    const profileImageLabel = document.getElementById('profile-image-label');
+    const img = document.createElement('img');
+    img.src = URL.createObjectURL(event.target.files[0]);
+    profileImageLabel.insertAdjacentElement('afterend', img)
 }
