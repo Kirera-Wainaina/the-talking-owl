b0a8297bd0e28faa276fb92b14220a1f7cfb91fb
Kirera Wainaina <richardwainainak@gmail.com>
2023-04-21 17:17:25 +0300

    check if preview image element exists before creating new one

--- frontend/js/admin/add-authors.js
+++ frontend/js/admin/add-authors.js
@@ -18,18 +18,28 @@ function attachListenerToFileButton() {
 }
 
 function displayProfileImage(event) {
+    const url = URL.createObjectURL(event.target.files[0]);
+    const element = document.getElementById('profile-image-preview');
+
+    if (element) {
+        element.src = url;
+    } else {
+        createImagePreview(url);
+    }
+}
+
+function createImagePreview(url) {
     const profileImageLabel = document.getElementById('profile-image-label');
     const img = document.createElement('img');
-    img.src = URL.createObjectURL(event.target.files[0]);
+    img.src = url;
     img.id = 'profile-image-preview';
-    profileImageLabel.insertAdjacentElement('afterend', img)
+    profileImageLabel.insertAdjacentElement('afterend', img)    
 }
 
 async function submitAuthor(event) {
     event.preventDefault();
     showSpinningIcon(document.querySelector('button[type="submit"]'));
 
-    // const formdata = await enterDetailsIntoFormdata(event.target);
     const formdata = new FormData(event.target);
     giveImageRandomName(formdata);
     formdata.append('fileNumber', 1);
