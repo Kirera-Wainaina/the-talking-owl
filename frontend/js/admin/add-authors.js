cc7fef521b5e093591c7c2cf67833cf78fc24230
Kirera Wainaina <richardwainainak@gmail.com>
2023-04-21 12:11:00 +0300

    send author data to backend for saving

--- frontend/js/admin/add-authors.js
+++ frontend/js/admin/add-authors.js
@@ -3,6 +3,9 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const fileInput = document.querySelector('input[type="file"]');
     fileInput.addEventListener('change', displayProfileImage);
+
+    const form = document.querySelector('form');
+    form.addEventListener('submit', submitAuthor)
 })
 
 function attachListenerToFileButton() {
@@ -18,4 +21,32 @@ function displayProfileImage(event) {
     img.src = URL.createObjectURL(event.target.files[0]);
     img.id = 'profile-image-preview';
     profileImageLabel.insertAdjacentElement('afterend', img)
+}
+
+async function submitAuthor(event) {
+    event.preventDefault();
+
+    // const formdata = await enterDetailsIntoFormdata(event.target);
+    const formdata = new FormData(event.target);
+    fetch('/api/admin/add-author.js', {
+        method: 'POST',
+        body: formdata
+    }).then(handleSubmitAuthorResponse)
+}
+
+async function enterDetailsIntoFormdata(form) {
+    const formdata = new FormData(form);
+    formdata.append('profileImage', await getImageBlob(form))
+
+    return formdata
+}
+
+function getImageBlob(form) {
+    const imageEl = form.querySelector('#profile-image-preview');
+    return fetch(imageEl.src)
+        .then(response => response.blob())
+}
+
+function handleSubmitAuthorResponse(response) {
+    console.log(response)
 }
