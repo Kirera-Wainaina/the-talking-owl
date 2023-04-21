50b51991e0e5c60ac8932e05c08b4564965320c4
Kirera Wainaina <richardwainainak@gmail.com>
2023-04-21 13:31:44 +0300

    verify admin password

--- api/admin/add-author.js
+++ api/admin/add-author.js
@@ -1,14 +1,26 @@
-const FormDataHandler = require('../../utils/formDataHandler')
+const FormDataHandler = require('../../utils/formDataHandler');
+const storage = require('../../utils/storage');
 
 exports.main = async function (request, response) {
     try {
         const [fields, files] = await new FormDataHandler(request).run();
+        if (await isAdminPassword(fields.adminPassword)) {
+            console.log(files)
+            console.log(fields)
+            const cloudFile = await storage.saveImage(files[0]);
+            const metadata = await storage.getFileMetadata(cloudFile);                
+        } else {
+            response.writeHead(403, {'content-type': 'text/html' });
+            response.end('forbidden');
+        }
 
-        console.log(fields)
-            
     } catch (error) {
         console.log(error);
         response.writeHead(500, { 'content-type': 'text/html'});
         response.end('error')
     }
+}
+
+function isAdminPassword(entry) {
+    return bcrypt.compare(entry, process.env.ADMIN_PASSWORD);
 }
