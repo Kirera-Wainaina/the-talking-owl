f1a5c6333381edeadb113dc949587b949ff9ee8b
Kirera Wainaina <richardwainainak@gmail.com>
2023-04-22 17:15:58 +0300

    send forbidden response if password doesnt match

--- api/admin/edit-author.js
+++ api/admin/edit-author.js
@@ -1,16 +1,22 @@
 const FormDataHandler = require('../../utils/formDataHandler');
 const storage = require('../../utils/storage');
 const imageHandler = require('../../utils/imageHandler');
+const fsPromises = require('fs/promises')
 
 exports.main = async function (request, response) {
     try {
         const [fields, files ] = await new FormDataHandler(request).run();
-        
-        if (files.length) {
-            const [ convertedFileMetadata ] = await imageHandler.minimizeImage(files[0]);
-            const cloudFile = await storage.saveImage(convertedFileMetadata.destinationPath);
-            const cloudFileMetadata = await storage.getFileMetadata(cloudFile);
-            console.log(cloudFileMetadata)
+        if (await isAdminPassword(fields.adminPassword)){
+            if (files.length) {
+                const [ convertedFileMetadata ] = await imageHandler.minimizeImage(files[0]);
+                const cloudFile = await storage.saveImage(convertedFileMetadata.destinationPath);
+                const cloudFileMetadata = await storage.getFileMetadata(cloudFile);
+                console.log(cloudFileMetadata)
+            }    
+        } else {
+            await fsPromises.unlink(files[0])
+            response.writeHead(403, {'content-type': 'text/plain' });
+            response.end('forbidden');
         }
         // carry out the rest of the process here not in if statement
         console.log(fields, files);
@@ -19,4 +25,8 @@ exports.main = async function (request, response) {
         response.writeHead(500, { 'content-type': 'text/plain' })
         response.end('error')
     }
+}
+
+function isAdminPassword(entry) {
+    return bcrypt.compare(entry, process.env.ADMIN_PASSWORD);
 }
